8b449f81a23a0baec5c863835e07d3d446ce54fb
hungt1 <nguyenvanhung.brvt.2002@gmail.com>
2021-11-04 01:10:19 +0700

    add solution

---
+++ CSES/Dynamic Programming/Coin Combinations I/sol.cpp
@@ -0,0 +1,48 @@
+/**
+ *    author:    hungt1
+ *    created:   04-11-2021   00:37:14
+**/
+#include <bits/stdc++.h>
+using namespace std;
+
+#define long long long
+#define db double
+#define pii pair <int, int>
+#define pll pair <long, long>
+#define all(c) c.begin(), c.end()
+#define fastio ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0)
+
+template <class T> inline void chmin(T &a, const T& val) { if (a > val) a = val; }
+template <class T> inline void chmax(T &a, const T& val) { if (a < val) a = val; }
+template <class T> long sqr(T a) { return 1ll * a * a; }
+template <class T> void compress(T &a){
+    sort(all(a)); a.resize(unique(all(a)) - a.begin());
+}
+
+const db pi = acos(-1.00);
+const db eps = 1e-6;
+const int INF = 2e9;
+const int MOD = 1e9 + 7;
+const int dx[4] = {0, 0, -1, 1};
+const int dy[4] = {-1, 1, 0, 0};
+
+int main()
+{
+    fastio;
+    int n, x; cin >> n >> x;
+    vector<int> dp(x + 1, 0);
+    vector<int> c(n);
+    for (int &val : c) cin >> val;
+    dp[0] = 1;
+    for (int i = 1; i <= x; i++){
+        for (int &val : c){
+            if (i >= val){
+                dp[i] += dp[i - val];
+                
+                dp[i] %= MOD;
+            }
+        }
+    }
+    cout << dp[x];
+    return 0;
+}
---
+++ CSES/Dynamic Programming/Grid Paths/sol.cpp
@@ -0,0 +1,50 @@
+/**
+ *    author:    hungt1
+ *    created:   04-11-2021   00:51:51
+**/
+#include <bits/stdc++.h>
+using namespace std;
+
+#define long long long
+#define db double
+#define pii pair <int, int>
+#define pll pair <long, long>
+#define all(c) c.begin(), c.end()
+#define fastio ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0)
+
+template <class T> inline void chmin(T &a, const T& val) { if (a > val) a = val; }
+template <class T> inline void chmax(T &a, const T& val) { if (a < val) a = val; }
+template <class T> long sqr(T a) { return 1ll * a * a; }
+template <class T> void compress(T &a){
+    sort(all(a)); a.resize(unique(all(a)) - a.begin());
+}
+
+const db pi = acos(-1.00);
+const db eps = 1e-6;
+const int INF = 2e9;
+const int MOD = 1e9 + 7;
+const int dx[4] = {0, 0, -1, 1};
+const int dy[4] = {-1, 1, 0, 0};
+
+int main()
+{
+    fastio;
+    int n; cin >> n;
+    vector<vector<char>> c(n, vector<char> (n));
+    for (int i = 0; i < n; i++){
+        for (int j = 0; j < n; j++){
+            cin >> c[i][j];
+        }
+    }
+    vector<vector<int>> dp(n + 1, vector<int> (n + 1, 0));
+    dp[0][1] = 1;
+    for (int i = 1; i <= n; i++){
+        for (int j = 1; j <= n; j++){
+            if (c[i - 1][j - 1] == '*') continue;
+            dp[i][j] += dp[i - 1][j] + dp[i][j - 1];
+            dp[i][j] %= MOD;
+        }
+    }
+    cout << dp[n][n];
+    return 0;
+}
