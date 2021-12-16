4975b00eaf51828604b9df4bd0adc89601928061
hungt1 <nguyenvanhung.brvt.2002@gmail.com>
2021-12-16 15:28:30 +0700

    add solution

---
+++ CSES/Graph Algorithms/Road Reparation/sol.cpp
@@ -0,0 +1,82 @@
+/**
+ *    author:    hungt1
+ *    created:   14-12-2021   10:42:58
+**/
+#include <bits/stdc++.h>
+using namespace std;
+
+#define long long long
+#define db double
+#define pii pair <int, int>
+#define pll pair <long, long>
+#define all(c) c.begin(), c.end()
+#define fastio ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0)
+
+template <class T> inline void chmin(T &a, const T& val) { if (a > val) a = val; }
+template <class T> inline void chmax(T &a, const T& val) { if (a < val) a = val; }
+template <class T> long sqr(T a) { return 1ll * a * a; }
+template <class T> void compress(T &a){
+    sort(all(a)); a.resize(unique(all(a)) - a.begin());
+}
+
+const db pi = acos(-1.00);
+const db eps = 1e-6;
+const int INF = 2e9;
+const int MOD = 1e9 + 7;
+const int dx[4] = {0, 0, -1, 1};
+const int dy[4] = {-1, 1, 0, 0};
+
+struct DSU {
+    vector<int> lab;
+    int n; 
+    
+    DSU(int n) : n(n) {
+        lab.resize(n + 1, -1);
+    }
+    
+    int get(int u){
+        return (lab[u] < 0 ? u : lab[u] = get(lab[u])); 
+    }
+    
+    int size(int u){
+        return -lab[get(u)];
+    }
+
+    bool same(int u, int v){
+        return get(u) == get(v);
+    }
+
+    bool unite(int x, int y){
+        x = get(x); y = get(y);
+        if (x == y) return 0;
+        if (lab[x] > lab[y]) swap(x, y);
+        lab[x] += lab[y]; lab[y] = x;
+        return 1;
+    }
+};
+
+struct pack {
+    int u, v, c;
+};
+
+int main()
+{
+    fastio;
+    int n, m; cin >> n >> m;
+    vector<pack> edges;
+    for (int i = 0; i < m; i++){
+        int u, v, c; cin >> u >> v >> c;
+        edges.push_back({u, v, c});
+    }
+    sort(all(edges), [&](const pack& cur, const pack& other){
+        return cur.c < other.c;
+    });
+    DSU dsu(n);
+    long ans = 0;
+    for (auto [u, v, c] : edges){
+        if (dsu.unite(u, v)) ans += c;
+    }
+    if (dsu.size(1) != n) cout << "IMPOSSIBLE\n";
+    else cout << ans << '\n';
+    return 0;
+}
